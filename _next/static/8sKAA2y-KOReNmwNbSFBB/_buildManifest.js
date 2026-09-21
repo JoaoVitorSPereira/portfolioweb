df906db14105d5604234b636c12cfe64df30b24f
JoaoVitorSPereira <57682027+JoaoVitorSPereira@users.noreply.github.com>
2026-09-21 11:39:01 +0000

    Deploying to gh-pages from @ JoaoVitorSPereira/portfolioweb@b7987131d283f6a2b7ac14a58877731642d213a0 🚀

--- _next/static/8sKAA2y-KOReNmwNbSFBB/_buildManifest.js
+++ _next/static/8sKAA2y-KOReNmwNbSFBB/_buildManifest.js
@@ -3,7 +3,7 @@ self.__BUILD_MANIFEST = {
     "static/chunks/3wr4q79g67wfd.js"
   ],
   "/[lang]": [
-    "static/chunks/0a9179z43odpx.js"
+    "static/chunks/1xif1t1ec14e9.js"
   ],
   "/_error": [
     "static/chunks/3ltfqz-pdkiwv.js"
